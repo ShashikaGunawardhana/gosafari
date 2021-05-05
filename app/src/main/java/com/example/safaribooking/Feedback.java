6bcf01074f14b9cdf5ae8516aed5dede4273ec7b
IT19980478 <80104127+IT19980478@users.noreply.github.com>
2021-05-05 12:05:00 +0530

    Create feedback code..

--- app/src/main/java/com/example/safaribooking/Feedback.java
+++ app/src/main/java/com/example/safaribooking/Feedback.java
@@ -3,12 +3,21 @@ package com.example.safaribooking;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class Feedback extends AppCompatActivity {
+
+    EditText comment;
+    Button sendbtn,dltbtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_feedback);
+
+        sendbtn = (Button)findViewById(R.id.d_feedbackBtn);
+        dltbtn = (Button)findViewById(R.id.d_deletefeed);
+        comment = (EditText) findViewById(R.id.d_feedInput);
     }
 }
